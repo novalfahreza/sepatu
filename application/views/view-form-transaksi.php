7e6d9433af0f2199896fb2182978ae2893d4d4d9
novalfahreza <maufal.f.persib@gmail.com>
2021-10-22 20:41:42 +0700

    tugas 7 pertemuan 6

--- application/views/view-form-transaksi.php
+++ application/views/view-form-transaksi.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-    <title>Tugas WebProgramming II_Naufal Fahreza_12200993</title>
+    <title>Tugas WebProgramming II_Septian Muhamad Putra_12200931</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="<?= base_url('assets/'); ?>css/stylebuku.css">
 </head>
